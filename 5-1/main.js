8ec448fb67c25f824afa1bc93f456934e751cf42
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-06-06 13:01:04 +0900

    5-1

--- 5-1/main.js
+++ 5-1/main.js
@@ -2,19 +2,26 @@ const profiles = {
   profiles: [
     {
       name: "佐藤",
-      age: 22
+      age: 22,
     },
     {
       name: "鈴木",
-      age: 34
+      age: 34,
     },
     {
       name: "高橋",
-      age: 42
+      age: 42,
     },
     {
       name: "田中",
-      age: 15
-    }
-  ]
-}
+      age: 15,
+    },
+  ],
+};
+
+const profilesArray = profiles.profiles.map((profile) => {
+  return `${profile.name}:${profile.age}`;
+});
+profilesArray.forEach((profile) => {
+  console.log(profile);
+});
