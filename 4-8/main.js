3baffd4f5b0e86e5e9b1bd4d278cc014b079ac73
Shin.Yamamoto <yamashin0123@outlook.jp>
2022-06-06 12:25:18 +0900

    4-8

--- 4-8/main.js
+++ 4-8/main.js
@@ -1,1 +1,14 @@
-const str = "こはるはるここはるここはるはるここはるこはるはるはる"
+const str = "こはるはるここはるここはるはるここはるこはるはるはる";
+
+let count = 0;
+let count2 = 0;
+for (let i = 0; i < str.length; i++) {
+  const threeString = str[i] + str[i + 1] + str[i + 2];
+  if (threeString === "こはる") {
+    count++;
+  } else if (threeString === "はるこ") {
+    count2++;
+  }
+}
+console.log(`こはる: ${count}`);
+console.log(`はるこ: ${count2}`);
